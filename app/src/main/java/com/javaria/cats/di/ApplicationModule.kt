3580be65accfdeb88a97ce7817928b507cf7d241
javariahazoor <javaria.hazoor@hotmail.com>
2021-02-08 10:51:45 +0100

    removing deprecated imports

--- app/src/main/java/com/javaria/cats/di/ApplicationModule.kt
+++ app/src/main/java/com/javaria/cats/di/ApplicationModule.kt
@@ -5,11 +5,11 @@ import com.javaria.cats.data.network.RemoteDataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object ApplicationModule {
     @Provides
     fun provideCatApi(): CatApi {
